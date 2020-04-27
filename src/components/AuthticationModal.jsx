bee6483e2a1b50f5720b4a143743c2ae80d2b0e6
Xezzon <yzjdx4399@gmail.com>
2020-04-27 19:48:13 +0800

    feat(authentication): 新增登录框组件

---
+++ src/components/AuthticationModal.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+/**
+ * 登录对话框
+ * 收到401响应后自动弹出
+ * 不允许以任何方式手动关掉对话框
+ * 登录成功后销毁
+ */
+function AuthenticationModal({ visible }) {
+    return <React.Fragment></React.Fragment>
+}
+
+export default AuthenticationModal;
