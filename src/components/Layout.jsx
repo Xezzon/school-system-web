9ad1f0fb11b073395df87123cfec6f2ba88ef25b
Xezzon <yzjdx4399@gmail.com>
2020-04-29 14:00:11 +0800

    feat(layout): 新增三种布局模式
    顶部导航布局、通栏布局、侧边导航布局。

---
+++ src/components/Layout.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import Layout from 'antd/es/layout';
+
+function BasicLayout({ children }) {
+    return (
+        <Layout>
+            <Layout.Header>
+                <span className="logo"></span>
+            </Layout.Header>
+            <Layout>
+                <Layout.Content>{children}</Layout.Content>
+                <Layout.Footer></Layout.Footer>
+            </Layout>
+        </Layout>
+    );
+}
+
+function BannerLayout({ menu, children }) {
+    return (
+        <Layout>
+            <Layout.Header>
+                <span className="logo"></span>
+            </Layout.Header>
+            <Layout>
+                <Layout.Sider>{menu}</Layout.Sider>
+                <Layout>
+                    <Layout.Content>{children}</Layout.Content>
+                    <Layout.Footer></Layout.Footer>
+                </Layout>
+            </Layout>
+        </Layout>
+    );
+}
+
+function SiderLayout({ menu, children }) {
+    return (
+        <Layout>
+            <Layout.Sider>
+                <div className="logo"></div>
+                {menu}
+            </Layout.Sider>
+            <Layout>
+                <Layout.Header></Layout.Header>
+                <Layout.Content>{children}</Layout.Content>
+                <Layout.Footer></Layout.Footer>
+            </Layout>
+        </Layout>
+    );
+}
+
+export { BasicLayout, BannerLayout, SiderLayout };
