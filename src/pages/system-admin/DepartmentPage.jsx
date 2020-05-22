1e171b29acdf009c6ccdfcba1bdef04650edac6a
Xezzon <yzjdx4399@gmail.com>
2020-05-22 14:17:34 +0800

    refactor: 将DepartmentTable抽取为单独的组件

--- src/pages/system-admin/DepartmentPage.jsx
+++ src/pages/system-admin/DepartmentPage.jsx
@@ -4,21 +4,32 @@ import Papa from 'papaparse';
 import staticModal from '@/hoc/staticModal';
 
 function DepartmentPage() {
-    let [page, setPage] = React.useState({ current: 1, pageSize: 15 });
-
-    let handleTableChange = (pagination, filter, sorter) => {
-        setPage({ ...page, current: pagination.current });
-    };
-    let handleExportCSV = () => {
-        let csv = 'id,部门名称,英文名,联系电话,email\n';
-        let download = document.createElement('a');
-        download.href = 'data:text/csv;utf-8,' + csv;
-        download.download = '部门表.csv';
-        download.click();
-    };
-    let handleImportCSV = (file) => {
-        Papa.parse(file, { complete: console.debug });
-    };
+    /* 测试数据START */
+    let dataSource = [
+        { id: '1', name: 'teaching', cname: '教务处', tel: '123456', email: '123@123.com' },
+        { id: '2', name: 'jiaotong', cname: '交通学院', tel: '123456', email: '123@123.com' },
+        { id: '1', name: 'teaching', cname: '教务处', tel: '123456', email: '123@123.com' },
+        { id: '2', name: 'jiaotong', cname: '交通学院', tel: '123456', email: '123@123.com' },
+        { id: '1', name: 'teaching', cname: '教务处', tel: '123456', email: '123@123.com' },
+        { id: '2', name: 'jiaotong', cname: '交通学院', tel: '123456', email: '123@123.com' },
+        { id: '1', name: 'teaching', cname: '教务处', tel: '123456', email: '123@123.com' },
+        { id: '2', name: 'jiaotong', cname: '交通学院', tel: '123456', email: '123@123.com' },
+        { id: '1', name: 'teaching', cname: '教务处', tel: '123456', email: '123@123.com' },
+        { id: '2', name: 'jiaotong', cname: '交通学院', tel: '123456', email: '123@123.com' },
+        { id: '1', name: 'teaching', cname: '教务处', tel: '123456', email: '123@123.com' },
+        { id: '2', name: 'jiaotong', cname: '交通学院', tel: '123456', email: '123@123.com' },
+        { id: '1', name: 'teaching', cname: '教务处', tel: '123456', email: '123@123.com' },
+        { id: '2', name: 'jiaotong', cname: '交通学院', tel: '123456', email: '123@123.com' },
+        { id: '1', name: 'teaching', cname: '教务处', tel: '123456', email: '123@123.com' },
+        { id: '2', name: 'jiaotong', cname: '交通学院', tel: '123456', email: '123@123.com' },
+        { id: '1', name: 'teaching', cname: '教务处', tel: '123456', email: '123@123.com' },
+        { id: '2', name: 'jiaotong', cname: '交通学院', tel: '123456', email: '123@123.com' },
+        { id: '1', name: 'teaching', cname: '教务处', tel: '123456', email: '123@123.com' },
+        { id: '2', name: 'jiaotong', cname: '交通学院', tel: '123456', email: '123@123.com' },
+        { id: '1', name: 'teaching', cname: '教务处', tel: '123456', email: '123@123.com' },
+        { id: '2', name: 'jiaotong', cname: '交通学院', tel: '123456', email: '123@123.com' },
+    ];
+    /* 测试数据END */
 
     return (
         <React.Fragment>
@@ -27,51 +38,42 @@ function DepartmentPage() {
                     <Input.Search></Input.Search>
                 </div>
                 <div className="table-toolkit-right">
-                    <Button onClick={handleExportCSV}>导出模板</Button>
-                    <Upload
-                        accept=".csv"
-                        showUploadList={false}
-                        beforeUpload={(file) => {
-                            handleImportCSV(file);
-                            return false;
-                        }}
-                    >
-                        <Button>批量导入</Button>
-                    </Upload>
-                    <Dropdown.Button
-                        overlay={
-                            <Menu>
-                                <Menu.Item>批量添加...</Menu.Item>
-                            </Menu>
-                        }
-                        trigger={['click']}
-                        onClick={DepartmentEditModal.show}
-                    >
-                        添加...
-                    </Dropdown.Button>
+                    <Button type="primary" onClick={DepartmentEditModal.show}>
+                        添加
+                    </Button>
                 </div>
             </div>
-            <Table
-                rowKey={(record) => record.id}
-                dataSource={dataSource}
-                pagination={page}
-                onChange={handleTableChange}
-            >
-                <Table.Column
-                    title="编号"
-                    key="index"
-                    render={(text, record, index) => (page.current - 1) * page.pageSize + (index + 1)}
-                />
-                <Table.Column title="部门名称" dataIndex="cname" />
-                <Table.Column title="电话" dataIndex="tel" />
-                <Table.Column title="email" dataIndex="email" />
+            <DepartmentTable dataSource={dataSource} operable />
+        </React.Fragment>
+    );
+}
+
+function DepartmentTable({ dataSource, operable }) {
+    let [page, setPage] = React.useState({ current: 1, pageSize: 15 });
+
+    let handleTableChange = (pagination, filter, sorter) => {
+        setPage({ ...page, current: pagination.current });
+    };
+
+    return (
+        <Table rowKey={(record) => record.id} dataSource={dataSource} pagination={page} onChange={handleTableChange}>
+            <Table.Column
+                title="编号"
+                key="index"
+                render={(text, record, index) => (page.current - 1) * page.pageSize + (index + 1)}
+            />
+            <Table.Column title="部门名称" dataIndex="cname" editable={true} />
+            <Table.Column title="英文名" dataIndex="name" />
+            <Table.Column title="电话" dataIndex="tel" editable={true} />
+            <Table.Column title="email" dataIndex="email" editable={true} />
+            <If condition={operable}>
                 <Table.Column
                     title="操作"
                     key="handlers"
                     render={(text, record) => (
                         <React.Fragment>
-                            <Button>设置管理员</Button>
-                            <Button onClick={() => DepartmentEditModal.show({ data: record })}>编辑</Button>
+                            <Button type="link">设置管理员</Button>
+                            <Button type="link">编辑</Button>
                             <Popconfirm
                                 title="确认删除？"
                                 onConfirm={() => {
@@ -80,13 +82,15 @@ function DepartmentPage() {
                                 okText="确认"
                                 cancelText="点错了"
                             >
-                                <Button danger>删除</Button>
+                                <Button type="link" danger>
+                                    删除
+                                </Button>
                             </Popconfirm>
                         </React.Fragment>
                     )}
                 />
+            </If>
             </Table>
-        </React.Fragment>
     );
 }
 
