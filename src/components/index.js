11c2b9f5858af1f71e9125958c7706c12dd09416
Xezzon <yzjdx4399@gmail.com>
2020-06-03 17:54:52 +0800

    bugfix: 更正组件名称

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,3 @@
-export { default as AuthenticationModal } from './AuthticationModal.jsx';
+export { default as AuthenticationModal } from './AuthenticationModal.jsx';
 export { default as Captcha } from './Captcha';
 export { default as PermissionTransfer } from './PermissionTransfer';
