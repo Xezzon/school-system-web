b513f68406d055f50d959428b9c70a370b361468
Xezzon <yzjdx4399@gmail.com>
2021-03-28 00:55:01 +0800

    refactor: 受控的CheckedCard组件

--- src/components/CheckedCard.jsx
+++ src/components/CheckedCard.jsx
@@ -1,20 +1,43 @@
-import { Badge, Card } from 'antd';
-import { useState } from 'react';
 import { CheckCircleTwoTone } from '@ant-design/icons';
+import { Badge, Card } from 'antd';
+import { useEffect, useRef, useState } from 'react';
 
-function CheckedCard({ children, option, ...props }) {
-    const [checked, setChecked] = useState(false);
+function CheckedCard({ checked: checkedProp = false, className, children, style, ...restProps }, ref) {
+    let [checked, setChecked] = useState(false);
+    let inputRef = useRef();
+    console.debug(1);
 
-    let toggleChecked = () => {
-        setChecked(!checked);
+    useEffect(() => {
+        console.debug(2);
+        setChecked(checkedProp);
+    }, [checkedProp]);
+
+    let change = (e) => {
+        let newChecked = e.target.checked;
+        restProps.onChange ? restProps.onChange(newChecked) : setChecked(newChecked);
+    };
+
+    let onClick = () => {
+        inputRef.current.click();
     };
 
     return (
-        <Badge count={<CheckCircleTwoTone style={{ visibility: checked || 'hidden' }} />} {...option}>
-            <Card {...props} hoverable style={checked ? { border: '1px solid #1890FF' } : {}} onClick={toggleChecked}>
-                {children}
-            </Card>
-        </Badge>
+        <>
+            <input
+                type="checkbox"
+                name={restProps.name}
+                checked={checked}
+                className="d-none"
+                readOnly
+                onChange={change}
+                ref={inputRef}
+            />
+            <Badge count={<CheckCircleTwoTone className={checked || 'invisible'} />}>
+                <Card hoverable style={checked ? { border: '1px solid #1890FF' } : {}} onClick={onClick}>
+                    {children}
+                </Card>
+            </Badge>
+        </>
     );
 }
 
