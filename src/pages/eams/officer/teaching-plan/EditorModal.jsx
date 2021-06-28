2a49ac6f43ed78cbf2109d7186b70a9f018ce3b7
xezzon <yzjdx4399@gmail.com>
2021-06-28 11:36:39 +0800

    feature: 编辑框loading状态

--- src/pages/eams/officer/teaching-plan/EditorModal.jsx
+++ src/pages/eams/officer/teaching-plan/EditorModal.jsx
@@ -1,5 +1,5 @@
 import axios from '@/services/axios';
-import { Form, Input, InputNumber, Modal, Select } from 'antd';
+import { Form, Input, InputNumber, Modal, Select, Spin } from 'antd';
 import { useEffect, useState } from 'react';
 
 function TeachingPlanEditorModal({ visible, courseId, onOk, onCancel }) {
@@ -7,6 +7,7 @@ function TeachingPlanEditorModal({ visible, courseId, onOk, onCancel }) {
     let [initValue, setInitValue] = useState({});
     let [teacherDict, setTeacherDict] = useState([]);
     let [courseTypeDict, setCourseTypeDict] = useState([]);
+    let [loading, setLoading] = useState(false);
 
     // 获取教师、教学类型字典
     useEffect(() => {
@@ -25,10 +26,16 @@ function TeachingPlanEditorModal({ visible, courseId, onOk, onCancel }) {
 
     let fetchInitValue = (courseId) => {
         if (courseId) {
-            axios.get(`/eams/teaching-plan/${courseId}`).then(({ data }) => {
-                data.profile = data.profile.split(';');
-                setInitValue(data);
-            });
+            setLoading(true);
+            axios
+                .get(`/eams/teaching-plan/${courseId}`)
+                .then(({ data }) => {
+                    data.profile = data.profile.split(';');
+                    setInitValue(data);
+                })
+                .finally(() => {
+                    setLoading(false);
+                });
         }
     };
 
@@ -69,45 +76,53 @@ function TeachingPlanEditorModal({ visible, courseId, onOk, onCancel }) {
     };
 
     return (
-        <Modal
-            visible={visible}
-            title="教学计划"
-            okText="提交"
-            cancelText="取消"
-            maskClosable={false}
-            onCancel={handleCancel}
-            onOk={handleFormSubmit}
-        >
-            <Form form={form} layout="vertical" initialValues={initValue}>
-                <Form.Item name="name" label="课程名" rules={[{ required: true }]}>
-                    <Input />
-                </Form.Item>
-                <Form.Item name="type" label="课程类别" rules={[{ required: true }]}>
-                    <Select>
-                        <For of={courseTypeDict} each="courseType">
-                            <Select.Option value={courseType.code} key={courseType.code}>
-                                {courseType.text}
-                            </Select.Option>
-                        </For>
-                    </Select>
-                </Form.Item>
-                <Form.Item name="profile" label="课程属性">
-                    <Select mode="tags" tokenSeparators={[';']} placeholder="多个属性请用英文半角分号;分隔"></Select>
-                </Form.Item>
-                <Form.Item name="credit" label="学分" rules={[{ required: true }]}>
-                    <InputNumber min={0} max={20} step={0.5} />
-                </Form.Item>
-                <Form.Item name={['teacher', 'id']} label="任课教师" rules={[{ required: true }]}>
-                    <Select>
-                        <For of={teacherDict} each="teacher">
-                            <Select.Option value={teacher.code} key={teacher.code}>
-                                {teacher.text}
-                            </Select.Option>
-                        </For>
-                    </Select>
-                </Form.Item>
-            </Form>
-        </Modal>
+        <>
+            <Modal
+                visible={visible}
+                title="教学计划"
+                okText="提交"
+                cancelText="取消"
+                maskClosable={false}
+                onCancel={handleCancel}
+                onOk={handleFormSubmit}
+            >
+                <Spin spinning={loading}>
+                    <Form form={form} layout="vertical" initialValues={initValue}>
+                        <Form.Item name="name" label="课程名" rules={[{ required: true }]}>
+                            <Input />
+                        </Form.Item>
+                        <Form.Item name="type" label="课程类别" rules={[{ required: true }]}>
+                            <Select>
+                                <For of={courseTypeDict} each="courseType">
+                                    <Select.Option value={courseType.code} key={courseType.code}>
+                                        {courseType.text}
+                                    </Select.Option>
+                                </For>
+                            </Select>
+                        </Form.Item>
+                        <Form.Item name="profile" label="课程属性">
+                            <Select
+                                mode="tags"
+                                tokenSeparators={[';']}
+                                placeholder="多个属性请用英文半角分号;分隔"
+                            ></Select>
+                        </Form.Item>
+                        <Form.Item name="credit" label="学分" rules={[{ required: true }]}>
+                            <InputNumber min={0} max={20} step={0.5} />
+                        </Form.Item>
+                        <Form.Item name={['teacher', 'id']} label="任课教师" rules={[{ required: true }]}>
+                            <Select>
+                                <For of={teacherDict} each="teacher">
+                                    <Select.Option value={teacher.code} key={teacher.code}>
+                                        {teacher.text}
+                                    </Select.Option>
+                                </For>
+                            </Select>
+                        </Form.Item>
+                    </Form>
+                </Spin>
+            </Modal>
+        </>
     );
 }
 
